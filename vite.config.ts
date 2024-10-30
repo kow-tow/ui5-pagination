c06e91f1fb6784c49753a96d5fa0723fe82cac1a
see-more <964489530@qq.com>
2024-10-30 20:01:33 +0800

    feat:use tsup build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import noderesolve from '@rollup/plugin-node-resolve'
 export default defineConfig({
     build: {
         lib: {
             entry:[resolve(__dirname,'src/index.ts')],
             name:'ui5-pagination',
             fileName:'ui5-pagination',
-            formats: ["es", "umd", "cjs"],
-        },
-        rollupOptions:{
-            plugins:[
-                noderesolve()
-            ]
+            formats: ["es"],
         }
     }
 })
