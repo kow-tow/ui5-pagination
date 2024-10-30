b1dca602ef07539793a3136fe1173e154ff1f131
☘ <kogawananari@gmail.com>
2024-10-30 20:10:59 +0800

    feat: :truck:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,93 @@
-import { Pagination } from "./ui5-pagination-lit";
+import { LitElement, html } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js'
+import { map } from 'lit/directives/map.js';
+import '@ui5/webcomponents/dist/Button.js'
+import '@ui5/webcomponents-icons/dist/AllIcons.js'
 
-export {
-    Pagination
+@customElement('ui5-pagination-lit')
+export class Pagination extends LitElement {
+  @property({ type: Number }) pageIndex: number = 0;
+  @property({ type: Number }) totalPage: number = 0;
+  @property({ type: Function }) onChange = (e: number) => { return e };
+  @state({})
+  private prevIsDisabled = this.totalPage == 0 || this.pageIndex == 0
+  @state({})
+  private nextIsDisabled = this.totalPage == 0 || this.pageIndex == this.totalPage - 1
+
+  private onClick(e: Event) {
+    if ((e.target as HTMLElement).tagName === "UI5-BUTTON") {
+      this.onChange(parseInt((e.target as HTMLElement).dataset.to!) + 1)
+      this.pageIndex = parseInt((e.target as HTMLElement).dataset.to!)
+    }
+  }
+  protected render() {
+    return html`
+      <span
+        @click=${this.onClick}
+      >
+          <ui5-button
+            design="Emphasized"
+            icon="navigation-left-arrow"
+            disabled=${this.prevIsDisabled}
+            data-to=${this.pageIndex - 1}
+        ></ui5-button>
+        ${map(Array(this.totalPage), (_, i) => {
+      if (i === this.pageIndex)
+        return html`
+              <ui5-button design="Emphasized" disabled>
+                ${i + 1}
+              </ui5-button>
+            `
+      if (Math.abs(i - this.pageIndex) <
+        Math.max(5 - this.pageIndex, 6 - this.totalPage + this.pageIndex, 2) ||
+        (i == 1 && this.pageIndex == 3) ||
+        (i == this.totalPage - 2 && this.pageIndex == this.totalPage - 4))
+        return html`
+            <ui5-button design="Default" data-to=${i}>
+                ${i + 1}
+            </ui5-button>`
+
+      if (i === 0)
+        return html`
+                <ui5-button design="Default" data-to="0">
+                    1
+                </ui5-button>
+            `
+      if (i === this.totalPage - 1)
+        return html`
+                <ui5-button design="Default" data-to=${i}>
+                    ${this.totalPage}
+                </ui5-button>
+            `
+      if (i === 1)
+        return html`
+                  <ui5-button design="Transparent" disabled>
+                      ...
+                  </ui5-button>
+              `
+      if (i === this.totalPage - 2)
+        return html`
+                  <ui5-button design="Transparent" disabled>
+                      ...
+                  </ui5-button>
+              `
+    })}
+        <ui5-button
+          design="Emphasized"
+          icon="navigation-right-arrow"
+          disabled=${this.nextIsDisabled}
+          data-to=${this.pageIndex + 1}
+        >
+        </ui5-button>
+      </span>
+      `
+  }
+
+
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ui5-pagination-lit': Pagination
+  }
 }
