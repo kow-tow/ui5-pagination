5d689e0f61606f0b77e462dd7da016803ae5d5f3
see-more <964489530@qq.com>
2024-11-05 16:37:53 +0800

    :sparkles:
    Co-authored-by: ☘ <kogawananari@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { LitElement, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { LitElement, PropertyValues, html } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js'
 import { map } from 'lit/directives/map.js'
 
 @customElement('ui5-pagination')
 export class Pagination extends LitElement {
     @property({ type: Number }) pageIndex: number = 0
     @property({ type: Number }) totalPage: number = 0
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     @property({ type: Function }) onChange = (_i: number) => {}
+    @state({})
+    pages: Array<number> = []
+    @state({})
+    current = 1
     private onClick(e: PointerEvent | MouseEvent) {
         const target = e.target as HTMLElement | null
         if (target?.tagName === 'UI5-BUTTON') {
@@ -21,64 +26,81 @@ export class Pagination extends LitElement {
                     }),
                 )
             )
-                this.onChange?.((this.pageIndex = to))
+                this.onChange?.((this.current = to))
         }
     }
+
+    protected firstUpdated(changed: PropertyValues): void {
+        if (changed.has('totalPage')) {
+            this.pages = Array.from({ length: this.totalPage }, (_, i) => i + 1)
+        }
+        if (changed.has('pageIndex')) {
+            if (this.pageIndex !== 0) this.current = this.pageIndex
+        }
+    }
+    protected updated(changed: PropertyValues): void {
+        if (changed.has('current')) {
+            this.pageIndex = this.current - 1
+        }
+    }
+
     protected render() {
         return html`<span
-            style="display:inline-flex;gap:4px"
+            style=" display: inline-flex;
+            gap: 4px;"
             @click=${this.onClick}
         >
             <ui5-button
                 design="Emphasized"
                 icon="navigation-left-arrow"
                 ?disabled="${this.totalPage == 0 || this.pageIndex == 0}"
-                data-to=${this.pageIndex - 1}
+                data-to=${this.current - 1}
             ></ui5-button>
-            ${map(Array(this.totalPage), (_, i) => {
-                if (i === this.pageIndex)
-                    return html`<ui5-button design="Emphasized" disabled
-                        >${i + 1}</ui5-button
-                    >`
+            ${map(this.pages, (p) => {
+                if (p === this.current)
+                    return html`<ui5-button design="Emphasized" disabled>
+                        ${p}
+                    </ui5-button>`
                 if (
                     this.totalPage < 8 ||
-                    Math.abs(i - this.pageIndex) <
+                    Math.abs(p - this.current) <
                         Math.max(
-                            5 - this.pageIndex,
-                            6 - this.totalPage + this.pageIndex,
+                            this.pages.at(5)! - this.current,
+                            this.current - this.pages.at(-6)!,
                             2,
                         ) ||
-                    (i == 1 && this.pageIndex == 3) ||
-                    (i == this.totalPage - 2 &&
-                        this.pageIndex == this.totalPage - 4)
+                    (p == this.pages.at(1) &&
+                        this.current == this.pages.at(3)) ||
+                    (p == this.pages.at(-2) &&
+                        this.current == this.pages.at(-4))
                 )
-                    return html`<ui5-button design="Default" data-to=${i}
-                        >${i + 1}</ui5-button
-                    >`
+                    return html` <ui5-button design="Default" data-to="${p}">
+                        ${p}
+                    </ui5-button>`
 
-                if (i === 0)
-                    return html`<ui5-button design="Default" data-to="0"
-                        >1</ui5-button
-                    >`
-                if (i === this.totalPage - 1)
-                    return html`<ui5-button design="Default" data-to=${i}
-                        >${this.totalPage}</ui5-button
-                    >`
-                if (i === 1)
-                    return html`<ui5-button design="Transparent" disabled
-                        >...</ui5-button
-                    >`
-                if (i === this.totalPage - 2)
-                    return html`<ui5-button design="Transparent" disabled
-                        >...</ui5-button
-                    >`
+                if (p === this.pages.at(0))
+                    return html`<ui5-button design="Default" data-to="1">
+                        1
+                    </ui5-button>`
+                if (p === this.pages.at(-1))
+                    return html`<ui5-button design="Default" data-to="${p}">
+                        ${p}
+                    </ui5-button>`
+                if (p === this.pages.at(1))
+                    return html`<ui5-button design="Transparent" disabled>
+                        ...
+                    </ui5-button>`
+                if (p === this.pages.at(-2))
+                    return html`<ui5-button design="Transparent" disabled>
+                        ...
+                    </ui5-button>`
             })}
             <ui5-button
                 design="Emphasized"
                 icon="navigation-right-arrow"
                 ?disabled="${this.totalPage == 0 ||
-                this.pageIndex == this.totalPage - 1}"
-                data-to=${this.pageIndex + 1}
+                this.current == this.totalPage}"
+                data-to=${this.current + 1}
             ></ui5-button>
         </span>`
     }
